52b47cc9e0a320b092c7d1c63172f7759d8f2328
Firecoding0 <codeapostol@gmail.com>
2024-12-31 17:51:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "7342782631:AAEaKF2dYJXPhgeJ-dRT1LQPtbC_J6R75iU"  # Вставьте сюда токен вашего бота
+TOKEN = ""  # Вставьте сюда токен вашего бота
 DOWNLOAD_PATH = "downloads/"  # Папка для загрузки файлов
